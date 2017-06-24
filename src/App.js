069bbfa3f5359849d334a0f58813220291e61dc0
Richard Kalehoff <richardkalehoff@gmail.com>
2017-06-23 17:03:41 -0700

    Add ContactList component
    First, we add the ContactList component, and then we use composition
    to by adding ContactList inside App's render method. Lastly we configure
    the components differently by passing different props.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class ContactList extends React.Component {
+  render() {
+    const people = this.props.contacts
+
+    return <ol>
+      {people.map(person => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContactList contacts={[
+          { name: 'Michael' },
+          { name: 'Ryan' },
+          { name: 'Tyler' }
+        ]}/>
+        <ContactList contacts={[
+          { name: 'Amanda' },
+          { name: 'Richard' },
+          { name: 'Geoff' }
+        ]}/>
       </div>
     );
   }
