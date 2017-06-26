4f7055abb1c197c1c5c968b472a643dedcb90ba1
Richard Kalehoff <richardkalehoff@gmail.com>
2017-06-26 13:07:07 -0700

    Use destructuring to clean up code

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -18,12 +18,15 @@ class ListContacts extends Component {
   }
 
   render() {
+    const { contacts, onDeleteContact } = this.props
+    const { query } = this.state
+
     let showingContacts
-    if (this.state.query) {
-      const match = new RegExp(escapeRegExp(this.state.query), 'i')
-      showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i')
+      showingContacts = contacts.filter((contact) => match.test(contact.name))
     } else {
-      showingContacts = this.props.contacts
+      showingContacts = contacts
     }
 
     showingContacts.sort(sortBy('name'))
@@ -35,7 +38,7 @@ class ListContacts extends Component {
             className='search-contacts'
             type='text'
             placeholder='Search contacts'
-            value={this.state.query}
+            value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
         </div>
@@ -49,7 +52,7 @@ class ListContacts extends Component {
                 <p>{contact.name}</p>
                 <p>{contact.email}</p>
               </div>
-              <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+              <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
                 Remove
               </button>
             </li>
