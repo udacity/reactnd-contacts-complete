f794f553e4937f6b2afaab2acdb14c623d8eb8c1
Richard Kalehoff <richardkalehoff@gmail.com>
2017-06-26 11:11:05 -0700

    Hook up delete button to remove contacts

--- src/App.js
+++ src/App.js
@@ -24,10 +24,15 @@ class App extends Component {
       }
     ]
   }
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+  }
   render() {
     return (
       <div>
-        <ListContacts contacts={this.state.contacts} />
+        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
       </div>
     )
   }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -12,7 +12,7 @@ function ListContacts (props) {
             <p>{contact.name}</p>
             <p>{contact.email}</p>
           </div>
-          <button className='contact-remove'>
+          <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
             Remove
           </button>
         </li>
