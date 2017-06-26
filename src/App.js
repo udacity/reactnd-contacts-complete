664306f50a05aafe47f4109860e00593fcbf0321
Richard Kalehoff <richardkalehoff@gmail.com>
2017-06-26 10:21:58 -0700

    Add default contacts and the ListContacts component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+]
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={contacts} />
       </div>
     )
   }
---
+++ src/ListContacts.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class ListContacts extends Component {
+  render() {
+    console.log('Props', this.props)
+    return (
+      <ol className='contact-list'>
+
+      </ol>
+    )
+  }
+}
+
+export default ListContacts
