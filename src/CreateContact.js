554bab12367719b1526900ea27b8bb60237aba0d
Richard Kalehoff <richardkalehoff@gmail.com>
2017-06-26 14:52:39 -0700

    Serialize new contact fields and pass to parent component

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
 
 class CreateContact extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const values = serializeForm(e.target, { hash: true })
+    if (this.props.onCreateContact)
+      this.props.onCreateContact(values)
+  }
+
   render() {
     return (
       <div>
         <Link className='close-create-contact' to='/'>Close</Link>
-        <form className='create-contact-form'>
+        <form onSubmit={this.handleSubmit} className='create-contact-form'>
           <ImageInput
             className='create-contact-avatar-input'
             name='avatarURL'
