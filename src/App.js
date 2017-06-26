151ea430aea71230811cd395ee08398df9b8f170
Richard Kalehoff <richardkalehoff@gmail.com>
2017-06-26 13:46:01 -0700

    Remove contact from database when deleted in the app

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ class App extends Component {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
+
+    ContactsAPI.remove(contact)
   }
   render() {
     return (
