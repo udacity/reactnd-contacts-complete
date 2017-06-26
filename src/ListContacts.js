eaa138af7d992f05449f524d514ac4224f736ae4
Richard Kalehoff <richardkalehoff@gmail.com>
2017-06-26 10:26:28 -0700

    Render default contacts

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 
 class ListContacts extends Component {
   render() {
-    console.log('Props', this.props)
     return (
       <ol className='contact-list'>
-
+        {this.props.contacts.map((contact) => (
+          <li key={contact.id}>
+            {contact.name}
+          </li>
+        ))}
       </ol>
     )
   }
