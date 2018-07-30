117d30139dc6833fbef292133ab949c0bd6added
Dimitra Karamperi <dkaramperi@yahoo.gr>
2018-07-30 23:41:30 +0300

    fix: Update app following the Udacity course

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ] 
+  }
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+  }
+
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
       </div>
     )
   }
---
+++ src/ListContacts.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function ListContacts (props) {
+    return (
+        <ol className = 'contact-list'>
+                {props.contacts.map((contact) => (
+                    <li key={contact.id} className='contact-list-item'>
+                        <div className='contact-avatar' style={{
+                        backgroundImage: `url(${contact.avatarURL})`
+                        }}/>
+                    <div className='contact-details'>
+                        <p>{contact.name}</p>
+                        <p>{contact.email}</p>
+                    </div>
+                    <button onClick={() => props.onDeleteContact(contact)}
+                        className='contact-remove'>Remove</button>
+                    </li>
+                ))}
+        </ol>
+    )
+}
+
+
+
+export default ListContacts
