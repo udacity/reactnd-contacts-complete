1992395eda4ac9f7c738c5f3472b5e0faf44f861
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2022-03-21 03:53:03 +0200

    update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
 aws s3api create-bucket --bucket trb123
 aws s3 cp --recursive --acl public-read ./build s3://trb123/
-aws s3 website s3://t-r-b/ --index-document index.html
+aws s3 website s3://trb123/ --index-document index.html
